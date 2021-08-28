5804ad0c1539682f5a35ad3ba5a438e3ff6e9661
nguyentup <nguyentup@gmail.com>
2021-08-27 18:17:45 -0700

    Initial Commit

---
+++ DentalQuiz/DentalQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  DentalQuizApp.swift
+//  DentalQuiz
+//
+//  Created by Teresa Nguyen on 8/27/21.
+//
+
+import SwiftUI
+
+@main
+struct DentalQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
