7837e0635fee47d034006413b73c197eb99129e6
nguyentup <nguyentup@gmail.com>
2021-08-29 16:08:32 -0700

    Changed to hygiene color theme

--- DentalQuiz/Views/ColorScheme.swift
+++ DentalQuiz/Views/ColorScheme.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct ColorScheme {
-    static let main = Color(red: 20/255, green: 28/255, blue: 58/255)
+    static let main = Color(red: 230/255, green: 230/255, blue: 250/255)
     static let accent = Color(red: 225/255, green: 182/255, blue: 193/255)
     static let correctGuess = Color.green
     static let incorrectGuess = Color.red
